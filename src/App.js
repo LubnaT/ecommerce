de8755053f738bb7e9fab22a854fb2daf00e9330
Lubna Tahreem <lubnatahreem789@gmail.com>
2023-12-11 11:08:31 +0530

    creating nav bar and carousel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import Navigation from './customer/components/Navigation/Navigation';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     Component
+     <Navigation/>
     </div>
   );
 }
