255881bd2a23f4680582b3c76cf47ff8c7cdcf99
Lubna Tahreem <lubnatahreem789@gmail.com>
2023-12-20 17:19:39 +0530

    creating product page

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import './App.css';
 import HomePage from './customer/pages/HomePage';
 import HomeSectionCard from './customer/components/HomeSectionCard/HomeSectionCard';
 import Footer from './customer/components/Footer/Footer';
+import ProductCard from './customer/components/Product/ProductCard';
+import Product from './customer/components/Product/Product';
 
 function App() {
   return (
     <div className="App">
   <Navigation/>
      <div>
-      <HomePage/>
+     <Product/>
+      {/* <HomePage/>
       <HomeSectionCard/>
      </div>
      <div>
-      <Footer/>
+      <Footer/> */}
+      
      </div>
     </div>
   );
--- src/customer/components/Product/ProductCard.jsx
+++ src/customer/components/Product/ProductCard.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import './ProductCard.css'
 
 const ProductCard = () => {
   return (
-    <div>ProductCard</div>
+    <div className='productCard w-[12rem] m-3 transition-all cursor-pointer'>
+        {/* image */}
+        <div className='h-[14rem]'>
+            <img className='h-full w-full object-cover object-left-top ' 
+            src='https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSvmd8Ig2O2-XspaQdzrnfBOyKI6Ll_5iB9Jop77zVB6VOqgq0UvGIjNQB7eBHo65MKm0EZU0WcZ6A8LsMXNYEKFPAa7F3xTQlK_Nm7NqPQvi78mFsle3zomw&usqp=CAc'></img>
+
+        </div>
+        {/* details */}
+        <div className='textPart bg-white text-sm max-w-max p-2'>
+            <div> <p className='font-bold opcity-60 w-max pl-2' >Universaloutitc</p>
+            <p className=''>Casual Pubt Sleeves Solid Women White Top</p>
+            </div>
+           {/* price */}
+        <div className=' text-sm flex items-center space-x-2'>
+            <p className='font-semibold'>Rs 199/-</p>
+            <p className='line-through opacity-50'>Rs 1999/-</p>
+            <p className='text-green-600 font-semibold'>70% off</p>
+        </div>
+        </div>
+        
+    </div>
   )
 }
 
