9f5cd290db343c666e74e42427f440572a73b1fa
Lubna Tahreem <lubnatahreem789@gmail.com>
2023-12-28 19:22:28 +0530

    done with button

--- src/customer/components/ProductDetails/ProductDetails.jsx
+++ src/customer/components/ProductDetails/ProductDetails.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import { StarIcon} from '@heroicons/react/20/solid'
 import { RadioGroup } from '@headlessui/react'
 import './ProductDetails.css'
-import { Rating } from '@mui/material'
+import { Button, Rating } from '@mui/material'
 
 const product = {
   name: 'Basic Tee 6-Pack',
@@ -37,14 +37,12 @@ const product = {
     { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
   ],
   sizes: [
-    { name: 'XXS', inStock: false },
-    { name: 'XS', inStock: true },
+   
     { name: 'S', inStock: true },
     { name: 'M', inStock: true },
     { name: 'L', inStock: true },
     { name: 'XL', inStock: true },
-    { name: '2XL', inStock: true },
-    { name: '3XL', inStock: true },
+    
   ],
   description:
     'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
@@ -152,41 +150,7 @@ export default function ProductDetails() {
             </div>
 
             <form className="mt-10">
-              {/* Colors */}
-              <div>
-                <h3 className="text-sm font-medium text-gray-900">Color</h3>
-
-                <RadioGroup value={selectedColor} onChange={setSelectedColor} className="mt-4">
-                  <RadioGroup.Label className="sr-only">Choose a color</RadioGroup.Label>
-                  <div className="flex items-center space-x-3">
-                    {product.colors.map((color) => (
-                      <RadioGroup.Option
-                        key={color.name}
-                        value={color}
-                        className={({ active, checked }) =>
-                          classNames(
-                            color.selectedClass,
-                            active && checked ? 'ring ring-offset-1' : '',
-                            !active && checked ? 'ring-2' : '',
-                            'relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none'
-                          )
-                        }
-                      >
-                        <RadioGroup.Label as="span" className="sr-only">
-                          {color.name}
-                        </RadioGroup.Label>
-                        <span
-                          aria-hidden="true"
-                          className={classNames(
-                            color.class,
-                            'h-8 w-8 rounded-full border border-black border-opacity-10'
-                          )}
-                        />
-                      </RadioGroup.Option>
-                    ))}
-                  </div>
-                </RadioGroup>
-              </div>
+            
 
               {/* Sizes */}
               <div className="mt-10">
@@ -249,13 +213,12 @@ export default function ProductDetails() {
                   </div>
                 </RadioGroup>
               </div>
+              {/* add to cart button */}
+              <div className='addToCartButton'>
+              <Button variant='contained' sx={{px:"2.5rem", py:"1rem", bgcolor:"#9c27b0"}}>Add to Cart</Button>
+              </div>
 
-              <button
-                type="submit"
-                className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                Add to bag
-              </button>
+            
             </form>
           </div>
 
