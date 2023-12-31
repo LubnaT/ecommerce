a45b3d1c29c075bdcecd92fcb3796ed337658f57
Lubna Tahreem <lubnatahreem789@gmail.com>
2023-12-31 20:22:40 +0530

    created a review card

---
+++ src/customer/components/ProductDetails/ProductReviewCard.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Grid,Box, Avatar, Rating } from "@mui/material";
+
+const ProductReviewCard = () => {
+  return (
+    <div>
+      <Grid container spacing={2} gap={3}>
+        <Grid item xs={1}>
+          <Box>
+            <Avatar
+              classname="text-white"
+              sx={{ width: 56, height: 56, bgcolor: "#515574" }}
+            >
+              R
+            </Avatar>
+          </Box>
+        </Grid>
+        <Grid item xs={9}>
+          <div classname="space-y-2">
+            <div>
+              <p>Raam </p>
+              <p>April 5, 2023</p>
+            </div>
+          </div>
+          <Rating value={4.5} name="half-rating"/>
+          <p>Nice Shirt, I love it </p>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default ProductReviewCard;
